f29693bdabc896b6632869f3cc20189d58ddbe33
JardGren <jaredcg1996@gmail.com>
2022-07-06 15:57:08 -0400

    Added gitignore and MyFirstDotnet

---
+++ MyFirstDotnet/MyFirstDotnet/Program.cs
@@ -0,0 +1,8 @@
+
+//This is a comment character/symbol
+
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine is from the System library, and allows us access to write a line of text to the console
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Who playin Elden Ring rn?");
